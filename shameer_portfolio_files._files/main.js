680ea84f35e8b76894c4cca3f0083b41be51ab67
shameer <ssat335@aucklanduni.ac.nz>
2017-06-17 00:17:46 +1200

    actual files added

---
+++ shameer_portfolio_files._files/main.js
@@ -0,0 +1,92 @@
+<!DOCTYPE html>
+<html lang="en">
+	
+	<head>
+	<meta charset="utf-8">
+	<meta name="viewport" width="device-width, initial-scale=1.0">
+	<meta name="description" content="">
+	<meta name="author" content="Shameer Sathar">
+	<!-- favicon -->
+	<link rel="shortcut icon" href="/img/logo.png">
+	<title>404 Error | Sorry, this page does not exist. | Aditya Sharma | Academic Website.</title>
+	<!-- styles -->
+	<link href="/css/bootstrap.css" rel="stylesheet">
+	<link href="/css/jumbotron-narrow.css" rel="stylesheet">
+	<link href="/css/style.css" rel="stylesheet">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+
+	<link rel="stylesheet" href="/css/fontello/css/fontello.css?rev=e45ccf726063531e8c3d2615e81be74f8a6f48b7">
+    	<link rel="stylesheet" href="/css/fontello/css/animation.css?rev=e45ccf726063531e8c3d2615e81be74f8a6f48b7">
+
+	<!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+	<!--[if lt IE 9]>
+  <script src="../../assets/js/html5shiv.js"></script>
+  <script src="../../assets/js/respond.min.js"></script>
+	<![endif]-->
+	<!-- google analytics - i will not share this data with google -->
+	<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-71449701-1', 'auto');
+  ga('send', 'pageview');
+
+</script>
+</head>
+
+	
+	<body>
+		<div class="container">
+			
+			<div class="header">
+	<ul class="nav nav-pills pull-right">
+		<li class=""><a href="/cv" title="CV">CV</a></li>
+		<!--<li class=""><a href="https://medium.com/ml-quest" title="ML.Quest Blog">ML.Quest-Blog</a></li> -->
+		<li class=""><a href="/notes" title="Notes">Notes</a></li>
+		<li class=""><a href="/contact" title="Contact">Contact</a></li>
+	</ul>
+	<h3 class="text-muted"><a href="/">Aditya Sharma</a></h3>
+</div> <!-- /header -->
+
+			
+			<div class="error">
+        	<h1>Whoah, that's a 404 Error</h1>
+        	<p class="lead">Hey, sorry, but the page you're trying to access doesn't exist. Try <a href="/" title="Aditya Sharma homepage">going back to the homepage</a>, or use the navigation menu above to find what you're looking for.</p>
+</div>
+
+			
+			<div class="footer">
+	<center>	
+		<div class="row">
+		    <p><i class="icon-cc"></i>&nbsp;2016&nbsp;&nbsp;Aditya&nbsp;Sharma</p>
+		    <p>
+		        <a class="creative-commons" href="http://creativecommons.org/licenses/by-nc-sa/4.0/">
+		            <i class="icon-cc-by"></i>
+		            <i class="icon-cc-nc"></i>
+		            <i class="icon-cc-sa"></i>
+		        </a>
+		    </p>
+		</div>
+	</center>
+</div> <!-- /footer -->
+
+		
+		</div> <!-- /container -->
+	
+	
+		<!-- javascript at the end of the doc so page loads faster -->
+<script src="http://code.jquery.com/jquery.js"></script>
+<script src="/js/bootstrap.min.js"></script>
+<script src="/js/library.js"></script>
+<script src="https://google-code-prettify.googlecode.com/svn/loader/run_prettify.js"></script>
+
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+    <script src="/vendor/autolinker/Autolinker.min.js?rev=e45ccf726063531e8c3d2615e81be74f8a6f48b7"></script>
+    <script src="/assets/js/main.js?rev=e45ccf726063531e8c3d2615e81be74f8a6f48b7"></script>
+
+	
+	</body>
+
+</html>
